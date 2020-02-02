87c068023be7349dd1ef2fd5beb2058f2f63c8dd
Collin Emery <collinemery@icloud.com>
2020-02-01 23:14:56 -0500

    Fixed prefix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Discord = require("discord.js");
-const token = '.';
+const prefix = '.';
 const client = new Discord.Client({disableEveryone:true})
 client.commands = new Discord.Collection();
 
