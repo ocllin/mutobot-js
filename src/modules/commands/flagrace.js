ecc72c012846de07eb0b7dd69ca75a62edc6fa49
ocllin <21271901+ocllin@users.noreply.github.com>
2020-03-08 17:50:55 -0400

    Clean up
    Removed debug/unnecessary documentation
    
    Removed flag races that already have happened today
    
    Added next race time to top

--- src/modules/commands/flagrace.js
+++ src/modules/commands/flagrace.js
@@ -14,32 +14,29 @@ module.exports = {
 		var timeNow = moment();
 		var string = "";
 		for(i = 0; i < races.length; i++){
-			if (timeNow.isSameOrAfter(races[i])){
+			if (!timeNow.isSameOrAfter(races[i])){
 				var duration = moment.duration(races[i].diff(timeNow));
-				// duration in hours
-				var hours = parseInt(duration.asHours()) + 23;
-				// duration in minutes
-				var minutes = parseInt(duration.asMinutes())%60 + 59;
-				var seconds = parseInt(duration.asSeconds())% 60 + 59;
-				string += "Race " + (i+1) + " in:\t" + hours + ' hrs '+ minutes+' mins and ' + seconds+' seconds\n';
-			}else{
-				var duration = moment.duration(races[i].diff(timeNow));
-				// duration in hours
 				var hours = parseInt(duration.asHours());
-
-				// duration in minutes
 				var minutes = parseInt(duration.asMinutes())%60;
 				var seconds = parseInt(duration.asSeconds())% 60;
+				//Prepare string for discord output
 				string += "Race " + (i+1) + " in:\t" + hours + ' hrs '+ minutes+' mins and ' + seconds+' seconds\n';
 			}
 		}
-		var newStr = string.slice(0, -1); //Slice last newline off of output
+
+		var racesStr = string.slice(0, -1); //Slice last newline off of output
+		var closestRace = racesStr.split('\n')[0];
 		//Prepare discord statement, formatting sucks :(
 		var flag =
     `
 \`\`\`css
+[Next Race]
+${closestRace}
+===============
 [Flag Race]
-${newStr}
+${racesStr}
+
+
 \`\`\`
 `;
 		message.channel.send(flag);
