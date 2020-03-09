534e5d3c81f5dee45d2f718c123b89e533484e7b
ocllin <21271901+ocllin@users.noreply.github.com>
2020-03-09 00:52:31 -0400

    Testing fix for no updates after first day

--- src/modules/commands/flagrace.js
+++ src/modules/commands/flagrace.js
@@ -1,17 +1,17 @@
 var moment = require('moment');
 
-//Collection of all timestamps for flag race events
-var races = [moment("12:00:00 pm", "HH:mm:ss a"), moment("07:00:00 pm", "HH:mm:ss a"), moment("09:00:00 pm", "HH:mm:ss a"),
-moment("10:00:00 pm", "HH:mm:ss a"), moment("11:00:00 pm", "HH:mm:ss a")];
-
 module.exports = {
 	name: 'flagrace',
 	description: 'Provides information flag race',
     aliases: ['flag', 'flagrace'],
 	execute(message) {
-		//Initialize times for flagraces every day
-		var d = new Date();
+		//Current time
 		var timeNow = moment();
+
+		//Collection of all timestamps for flag race events
+		var races = [moment("12:00:00 pm", "HH:mm:ss a"), moment("07:00:00 pm", "HH:mm:ss a"), moment("09:00:00 pm", "HH:mm:ss a"),
+		moment("10:00:00 pm", "HH:mm:ss a"), moment("11:00:00 pm", "HH:mm:ss a")];
+
 		var string = "";
 		for(i = 0; i < races.length; i++){
 			if (!timeNow.isSameOrAfter(races[i])){
